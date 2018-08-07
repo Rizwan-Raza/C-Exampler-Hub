de1b80b60408f23e23392852ade1a182fb208dde
Rizwan Raza <rizwan.raza987@gmail.com>
2018-08-07 22:29:37 +0530

    Execuables Added

--- Runner.bat
+++ Runner.bat
@@ -5,6 +5,7 @@ REM runs only if gcc command is added to the path variable
 :label
 cls
 gcc -Irizzu Rizwan.c -o Rizwan
+pause 
 Rizwan
 echo Want to Run it again ? If Yes, Press any key else Ctrl+C or close this window
 pause >nul
--- rizzu/MENU4.C
+++ rizzu/MENU4.C
@@ -1,6 +1,6 @@
 //Global variables
 extern int x[50],y[50],i,j,n,ch2;
-char aa[50][50],som[8]="Salary",eos[10]="Employee",filename[32];
+char aa[50][50],som[8]="Salary",eos[10]="Employee",filename[32], dirname[36]="DBs/";
 float *sal;
 FILE *ptr;
 void display_data();
@@ -22,6 +22,7 @@ void open_db();
 void manu_selection();
 int confirmed();
 void standard(int);
+char *trimwhitespace(char *);
 
 void menu4()
 {
@@ -189,6 +190,7 @@ void new_db()
     printf("Enter File Name with extension : ");
     fflush(stdin);
     gets(filename);
+	strcat(dirname, filename);
 	clrpnl(4,49,6,10);
 	gotoxy(4,6);
 	printf("Enter number of Students: ");
@@ -224,65 +226,64 @@ void new_db()
 	gotoxy(40,10);
 	printf("MARKS");
 	display_data();
-    do
-    {
-        ptr=fopen(filename,"w");
+        ptr=fopen(dirname,"w");
         if(!ptr)
         {
             gotoxy(4,7);
-            printf("Sorry you can't create file with this name");
+            printf("Sorry you can't create file with this name\a\a\a");
         }
-    }
-    while(!ptr);
     store_data(ptr);
     fclose(ptr);
     gotoxy(52,6);
-    printf("The Database has been stored in %s file.");
-    gotoxy(52,7);
-    printf("You can read and modify it using our another features");
+    printf("The Database has been stored");
+	gotoxy(52,7);
+	printf("in %s file.", filename);
+    gotoxy(52,9);
+    printf("You can read and modify it");
+    gotoxy(52,10);
+	printf("using our another features");
     getch();
 }
 void open_db()
 {
     drawSet();
-	drawBox(9,2,60,1);
+	drawBox(9,2,61,1);
 	gotoxy(11,3);
 	textcolor(BRDR);
 	printf("Main Menu ");
 	printf("%c Operation on Database and Files ",16);
 	textcolor(MENU);
-	printf("%c New Database",16);
+	printf("%c Open Database",16);
 	textbackground(BACK);
 	textcolor(TXET);
 	gotoxy(4,6);
 	printf("Enter File Name with extension : ");
 	fflush(stdin);
 	gets(filename);
-	do
-    {
-	ptr=fopen(filename,"r");
+	strcpy(dirname, "DBs/");
+	strcat(dirname, filename);
+	ptr=fopen(dirname,"r");
 	if(!ptr)
     {
         gotoxy(4,7);
-        printf("Sorry we can't find file %s in current directory",filename);
-    }
-    }
-	while(!ptr);
-	retrieve_data(ptr);
-	clrpnl(4,49,8,20);
-	gotoxy(4,8);
-	printf("The Entered Data is...");
-	gotoxy(4,10);
-	printf("S.N0");
-	gotoxy(10,10);
-	printf("NAME");
-	gotoxy(30,10);
-	printf("AGE");
-	gotoxy(40,10);
-	printf("MARKS");
-	display_data();
-    //store_data(ptr);
-    fclose(ptr);
+        printf("Sorry we can't find file %s in ./DBs directory",filename);
+    } else {
+		retrieve_data(ptr);
+		clrpnl(4,49,8,20);
+		gotoxy(4,8);
+		printf("The Entered Data is...");
+		gotoxy(4,10);
+		printf("S.N0");
+		gotoxy(10,10);
+		printf("NAME");
+		gotoxy(30,10);
+		printf("AGE");
+		gotoxy(40,10);
+		printf("MARKS");
+		display_data();
+		//store_data(ptr);
+		fclose(ptr);
+	}
     getch();
 }
 
@@ -330,7 +331,7 @@ void manu_selection()
 }
 void display_data()
 {
-	clrpnl(4,49,11,20);
+	clrpnl(4,49,11,20);	
 	for(i=0;i<n;i++)
 	{
 		gotoxy(4,11+i);
@@ -345,15 +346,16 @@ void display_data()
 }
 void store_data(FILE *output)
 {
+	fprintf(output, "%2d\n", n);
 	for(i=0;i<n;i++)
 	{
-        fprintf(output,"%2d. ",i+1);
-		fseek(output,2,SEEK_CUR);
-		fprintf(output,"%-25s",aa[i]);
-		fseek(output,2,SEEK_CUR);
-		fprintf(output,"%-2d",x[i]);
-		fseek(output,2,SEEK_CUR);
-		fprintf(output,"%-3.2f",sal[i]);
+        // fprintf(output,"%2d. ",i+1);
+		// fseek(output,2,SEEK_CUR);
+		fprintf(output,"%s ",aa[i]);
+		// fseek(output,2,SEEK_CUR);
+		fprintf(output,"%d ",x[i]);
+		// fseek(output,2,SEEK_CUR);
+		fprintf(output,"%.2f",sal[i]);
 		fprintf(output,"\n");
 	}
 	fclose(output);
@@ -365,16 +367,22 @@ void retrieve_data(FILE *output)
         printf("Sorry Can't Access file");
         return;
     }
+	fscanf(output, "%2d", &n);
 	for(i=0;i<n;i++)
 	{
-		/*fseek(output,6,SEEK_CUR);
-		fgets(aa[i],25,output);
-		fseek(output,3,SEEK_CUR);
-		fscanf(output,"%2d",&x[i]);
-		fseek(output,2,SEEK_CUR);
-		fscanf(output,"%f",&sal[i]);
-		fseek(output,2,SEEK_CUR);*/
-        printf("%c",getc(output));
+		// fseek(output,1,SEEK_CUR);
+		fscanf(output, "%s",aa[i]);
+		fscanf(output, "%d", &x[i]);
+		fscanf(output, "%f", &sal[i]);
+		// fgets(aa[i],25,output);
+		// fseek(output,3,SEEK_CUR);
+		// fscanf(output,"%2d",&x[i]);
+		// fseek(output,2,SEEK_CUR);
+		// fscanf(output,"%f",&sal[i]);
+		// fseek(output,2,SEEK_CUR);
+		//*aa[i] = trimwhitespace(aa[i]);
+		// printf("\a");
+        //printf("%c",getc(output));
 	}
 }
 int confirmed()
@@ -419,5 +427,11 @@ int confirmed()
             confirmed();
     }
     return 0;
+}
+
+char *trimwhitespace(char *str)
+{
+  while(isspace((unsigned char)*str)) str++;
+  return str;
 }
 ////////////////////////////////////////////////////////////////////
